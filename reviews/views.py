90e098ccdddad67c4243281acb7a74ba9785503d
Tibelson <elvistiburu17@gmail.com>
2025-10-27 23:24:30 +0000

    template working

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from .models import Book
+
+def welcome_view(request):
+    message = f"<html><h1>Welcome to Bookr! </h><p>{Book.objects.count()} books and counting! </p></html>"
+    return HttpResponse(message)
