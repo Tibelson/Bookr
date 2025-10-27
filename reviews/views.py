eb1907d7042ad50a8e3e95df6725b2c17f4a7bec
Tibelson <elvistiburu17@gmail.com>
2025-10-27 23:53:01 +0000

    updated settings to work with my_template

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-from .models import Book
+
 
 def welcome_view(request):
-    message = f"<html><h1>Welcome to Bookr! </h><p>{Book.objects.count()} books and counting! </p></html>"
-    return HttpResponse(message)
+   return render(request, 'base.html')
